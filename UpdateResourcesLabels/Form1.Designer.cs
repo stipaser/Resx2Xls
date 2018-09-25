a6d72b278f689e1196e88e73812c96c246bfe4fb
Sergiu Stipanov <sergiu.stipanov@outlook.com>
2018-09-25 14:12:33 +0300

    add count to same, null, different resources

--- UpdateResourcesLabels/Form1.Designer.cs
+++ UpdateResourcesLabels/Form1.Designer.cs
@@ -35,10 +35,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(181, 87);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(136, 71);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,30 +45,31 @@
             // 
             // rtbMissedKeys
             // 
-            this.rtbMissedKeys.Location = new System.Drawing.Point(23, 148);
+            this.rtbMissedKeys.Location = new System.Drawing.Point(17, 136);
+            this.rtbMissedKeys.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rtbMissedKeys.Name = "rtbMissedKeys";
-            this.rtbMissedKeys.Size = new System.Drawing.Size(1458, 645);
+            this.rtbMissedKeys.Size = new System.Drawing.Size(1105, 509);
             this.rtbMissedKeys.TabIndex = 1;
             this.rtbMissedKeys.Text = "";
             // 
             // lbProcess
             // 
             this.lbProcess.AutoSize = true;
-            this.lbProcess.Location = new System.Drawing.Point(29, 128);
+            this.lbProcess.Location = new System.Drawing.Point(22, 104);
+            this.lbProcess.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbProcess.Name = "lbProcess";
-            this.lbProcess.Size = new System.Drawing.Size(70, 17);
+            this.lbProcess.Size = new System.Drawing.Size(53, 13);
             this.lbProcess.TabIndex = 2;
             this.lbProcess.Text = "lbProcess";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1528, 825);
+            this.ClientSize = new System.Drawing.Size(1146, 670);
             this.Controls.Add(this.lbProcess);
             this.Controls.Add(this.rtbMissedKeys);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- UpdateResourcesLabels/Form1.cs
+++ UpdateResourcesLabels/Form1.cs
@@ -9,100 +9,29 @@ namespace UpdateResourcesLabels
 {
    public partial class Form1 : Form
    {
-      private const string basepath =
-          //  @"C:\Users\Pavel.martiniuc\Desktop\Resx2Xls_source\Resx2Xls\UpdateResourcesLabels\bin\Debug\";
-          @"D:\VsStudioProjects\LoxySoft\WFM-ProScheduler";
       private static StringBuilder missedKeys = new StringBuilder();
-      private static XElement element;
+      private static XElement ResourceElement;
       private static string resourceFile;
-      public Form1()
+       private static int countDif = 0;
+       private static int countNull = 0;
+       private static int countAll = 2;
+       private static int countTheSame = 0;
+
+        public Form1()
       {
          InitializeComponent();
       }
 
-      public static void UpdateResourceByKey(string pathToResources, string resourceFile, string key, string value)
-      {
-
-         if (element == null)
-         {
-            resourceFile = pathToResources + resourceFile;
-            element = XElement.Load(resourceFile);
-         }
-         else
-         {
-            if (resourceFile != pathToResources + resourceFile)
-            {
-               resourceFile = pathToResources + resourceFile;
-               element = XElement.Load(resourceFile);
-            }
-         }
-
-
-         XElement val = element.Descendants("data").FirstOrDefault(el => el.Attribute("name").Value == key);
-         if (val == null)
-         {
-            missedKeys.AppendLine(resourceFile + " | " + key + " | " + value);
-            //XElement newElement = new XElement("data",
-            //    new XAttribute("name", key),
-            //    new XAttribute(XNamespace.Xml + "space", "preserve"),
-            //    new XElement("value", value));
-            // element.Add(newElement);
-
-         }
-         else
-         {
-            // val.Element("value").Value = value;
-            if (val.Element("value").Value != value)
-            {
-               missedKeys.AppendLine(resourceFile + " | " + key + " | " + value);
-            }
-         }
-         ///   element.Save(resourceFile);
-
-      }
-
       private void button1_Click(object sender, EventArgs e)
       {
-         // reqdXlRange(basepath + "ProScheduler_ResourcesForBrandReview_v3.xlsx", "A5", "E71", @"D:\VsStudioProjects\LoxySoft\WFM-ProScheduler\XLScheduler\ProScheduler\Resources\");
-         // reqdXlRange(basepath + "ProScheduler_ResourcesForBrandReview_v3.xlsx", "A78", "E80", @"D:\VsStudioProjects\LoxySoft\WFM-ProScheduler\XLScheduler\ProScheduler.Wpf\Resources\");
-         // reqdXlRange(basepath + "ProScheduler_ResourcesForBrandReview_v3.xlsx", "A85", "E103", @"D:\VsStudioProjects\LoxySoft\WFM-ProScheduler\ProSchedulerWeb\ProScheduler.WebUI\");
-         UpdateGermanResources(
-             @"D:\TASKS\REVIEWS\565_German_translation\translations.xlsx",
-             "A3", "D5708", @"D:\PROJECTS\WFM-PROJECTS\WFM-ProScheduler");
-      }
+            // reqdXlRange(basepath + "ProScheduler_ResourcesForBrandReview_v3.xlsx", "A5", "E71", @"D:\VsStudioProjects\LoxySoft\WFM-ProScheduler\XLScheduler\ProScheduler\Resources\");
+            // reqdXlRange(basepath + "ProScheduler_ResourcesForBrandReview_v3.xlsx", "A78", "E80", @"D:\VsStudioProjects\LoxySoft\WFM-ProScheduler\XLScheduler\ProScheduler.Wpf\Resources\");
+            // reqdXlRange(basepath + "ProScheduler_ResourcesForBrandReview_v3.xlsx", "A85", "E103", @"D:\VsStudioProjects\LoxySoft\WFM-ProScheduler\ProSchedulerWeb\ProScheduler.WebUI\");
 
+          var pathToXls = @"D:\TASKS\REVIEWS\565_German_translation\translations.xlsx";
+          var pathToResources = @"D:\PROJECTS\WFM-PROJECTS\WFM-ProScheduler";
 
-      public void reqdXlRange(string pathToXls, string startCoordinates, string endCoordinates, string pathToResources)
-      {
-
-         Excel.Application app = new Excel.Application();
-         Excel.Workbook theWorkbook = app.Workbooks.Open(
-             pathToXls, 0, true, 5,
-             "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false,
-             0, true);
-         Excel.Sheets sheets = theWorkbook.Worksheets;
-         Excel.Worksheet worksheet = (Excel.Worksheet)sheets.get_Item(1);
-
-         Excel.Range range = worksheet.get_Range(startCoordinates, endCoordinates);
-         System.Array myvalues = (System.Array)range.Cells.Value;
-         Array strArray = myvalues;
-
-         var arr = strArray as object[,];
-         var len = arr.GetLength(0);
-
-         for (int i = 1; i <= len; i++)
-         {
-            var resourceFile = (string)arr[i, 1];
-            var key = (string)arr[i, 2];
-            var oldValue = (string)arr[i, 3];
-            var newValue = (string)arr[i, 4];
-            UpdateResourceByKey(pathToResources, resourceFile, key, newValue);
-         }
-
-         app.Quit();
-
-         GC.Collect();
-
+          UpdateGermanResources(pathToXls, "A3", "D5708", pathToResources);
       }
 
       public void UpdateGermanResources(string pathToXls, string startCoordinates, string endCoordinates, string pathToResources)
@@ -111,37 +40,87 @@ namespace UpdateResourcesLabels
          Excel.Application app = new Excel.Application();
          Excel.Workbook theWorkbook = app.Workbooks.Open(
              pathToXls, 0, true, 5,
-             "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false,
-             0, true);
+             "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true);
          Excel.Sheets sheets = theWorkbook.Worksheets;
-         Excel.Worksheet worksheet = (Excel.Worksheet)sheets.get_Item(1);
+         Excel.Worksheet worksheet = (Excel.Worksheet)sheets.Item[1];
 
-         Excel.Range range = worksheet.get_Range(startCoordinates, endCoordinates);
-         System.Array myvalues = (System.Array)range.Cells.Value;
-         Array strArray = myvalues;
-
-         var arr = strArray as object[,];
-         var len = arr.GetLength(0);
+         Excel.Range range = worksheet.Range[startCoordinates, endCoordinates];
+         Array myvalues = (Array)range.Cells.Value;
+         
+         var arr = myvalues as object[,];
+         
          missedKeys = new StringBuilder();
-         for (int i = 1; i <= len; i++)
+
+         for (int i = 1; i <= arr.GetLength(0); i++)
          {
             var resourceFile = (string)arr[i, 1];
             var key = (string)arr[i, 2];
             var oldValue = (string)arr[i, 3];
-            var newValue = (string)arr[i, 4];
+            var newValue = (string)arr[i, 4].ToString();
+            
             UpdateResourceByKey(pathToResources, resourceFile, key, newValue);
 
-            lbProcess.Text = (Convert.ToInt32((i / len)) * (int)100).ToString();
+            lbProcess.Text = (Convert.ToInt32((i / arr.GetLength(0))) * 100).ToString();
          }
 
          app.Quit();
 
          GC.Collect();
-         rtbMissedKeys.Text = missedKeys.ToString();
+
+          missedKeys.AppendLine("countTheSame " + countTheSame + " countNull " + countNull + " | " + " countDif " + countDif + " | " + " countAll " + countAll);
+          rtbMissedKeys.Text = missedKeys.ToString();
       }
 
-   }
+
+       public static void UpdateResourceByKey(string pathToResources, string resourceFile, string key, string valueFromXls)
+       {
+
+           if (ResourceElement == null)
+           {
+               resourceFile = pathToResources + resourceFile;
+               ResourceElement = XElement.Load(resourceFile);
+               countAll += 1;
+           }
+           else
+           {
+               if (resourceFile != pathToResources + resourceFile)
+               {
+                   resourceFile = pathToResources + resourceFile;
+                   ResourceElement = XElement.Load(resourceFile);
+                   countAll += 1;
+                }
+           }
 
 
+           XElement val = ResourceElement.Descendants("data").FirstOrDefault(el => el.Attribute("name")?.Value == key);
 
+           if (val == null) // is not in resource file
+           {
+               missedKeys.AppendLine(countAll +" | NULL | " + resourceFile + " | " + key + " | " + valueFromXls);
+
+                //XElement newElement = new XElement("data",
+                //    new XAttribute("name", key),
+                //    new XAttribute(XNamespace.Xml + "space", "preserve"),
+                //    new XElement("value", value));
+                // element.Add(newElement);
+               countNull += 1;
+
+           }
+           else // diferent
+           {
+               if (val.Element("value")?.Value != valueFromXls)
+               {
+                   missedKeys.AppendLine(countAll + " | DIF | " + resourceFile + " | " + key + " | " + valueFromXls);
+                   countDif += 1;
+               }
+               else
+               {
+                   // missedKeys.AppendLine(countAll + " | SAME | " + resourceFile + " | " + key + " | " + valueFromXls);
+                   countTheSame += 1;
+               }
+
+           }
+           ///   element.Save(resourceFile);
+       }
+    }
 }
